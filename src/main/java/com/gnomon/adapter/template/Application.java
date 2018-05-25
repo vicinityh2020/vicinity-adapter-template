fb1d776061664a1d858d1216ac9da7e4b5f03d0b
Dimitris Zandes <d.zandes@gnomon.com.gr>
2018-05-25 14:07:22 +0300

    Application startup class modification to adapt with multi tenant agent

--- src/main/java/com/gnomon/adapter/template/Application.java
+++ src/main/java/com/gnomon/adapter/template/Application.java
@@ -5,8 +5,21 @@
  */
 package com.gnomon.adapter.template;
 
+import com.gnomon.adapter.template.pojos.Response;
+import com.gnomon.adapter.template.services.AdapterService;
+import com.gnomon.adapter.template.services.impl.AdapterServiceImpl;
+
+import java.net.URI;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 /**
  *
@@ -17,6 +30,30 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     
     public static void main(String[] args){
+        
         SpringApplication.run(Application.class, args);
+        
+        RestTemplate restTemplate = new RestTemplate();
+        
+        /*
+        * Agent_URL:Agent_Port is where your multi tenant agent is running, e.g. 160.43.33.111:8888
+        *   , therefore, you have to adapt it for your case
+        *
+        * The POST during startup to the Agent below is needed when/if we do not have Auto-Discovery
+        */
+        String agentUrl = "http://<Agent_URL>:<Agent_Port>/agent/objects";
+        
+        AdapterService service = new AdapterServiceImpl();
+        Response resp_ = service.getThingsDescription();
+        
+        HttpEntity<String> bodyEntity = new HttpEntity<>(resp_.getMessage());
+        
+        URI uri = UriComponentsBuilder.fromHttpUrl(agentUrl)
+                                .build()
+                                .encode().toUri();
+        
+        ResponseEntity<String> response = restTemplate.exchange(uri, HttpMethod.POST, bodyEntity, String.class);
+        
+        System.out.println("Response status : " + response.getStatusCodeValue());
     }
 }
